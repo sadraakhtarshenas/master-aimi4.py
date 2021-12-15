0ae5714d3c4665ec059b581e9228e9bae566bbd6
sadraakhtarshenas <96003970+sadraakhtarshenas@users.noreply.github.com>
2021-12-15 12:30:18 -0800

    Add files via upload

---
+++ Master Aimi4.py
@@ -0,0 +1,28 @@
+
+
+name = input()
+family = input()
+age = int(input())
+city = input()
+average = int(input())
+print("hello", name, family,"you are", age, "old", "you live in", city, "and your avrage",average)
+
+
+
+
+name = input()
+family = input()
+age = int(input())
+city = input()
+average = float(input())
+print("hello", name, family,"you are", age, "old", "you live in", city, "and your avrage",average)
+
+
+
+
+name = input("enter your name: ")
+family = input("enter your family: ")
+age = int(input("enter your ege as number: "))
+city = input("enter your city: ")
+average = int(input("enter your average as number: "))
+print("hello", name, family,"you are", age, "old", "you live in", city, "and your avrage",average)
